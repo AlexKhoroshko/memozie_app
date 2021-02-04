11b3961e229cc2d033a85e0fdf81b2dcf3228690
Оleksandr Khoroshko <o_khoroshko@indiantvcompany.com>
2021-02-05 00:09:38 +0200

    fix card validation

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,6 @@ class Card < ApplicationRecord
   belongs_to :deck
   mount_uploader :image, ImageUploader
 
-  validates :front, :back, :image, presence: :true
+  validates :front, :back, presence: :true
   enum status: { right: 0, wrong: 1 }
 end
