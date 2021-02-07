408c04a398c2fdbde76f897cb858f9ee735826e0
Andrii Taranik <Andrii_Taranik@epam.com>
2021-02-07 12:18:20 +0200

    add_scope

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,7 @@
 class Card < ApplicationRecord
   belongs_to :deck
   mount_uploader :image, ImageUploader
+  scope :active, -> { where(status: 'active') }
 
   enum status: { active: 0, inactive: 1 }
 end
